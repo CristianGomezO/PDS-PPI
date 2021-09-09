52a409e7c6b6c4cfa039dbf74554bcd0e705638a
Andres Asprilla <andres_asprilla23171@elpoli.edu.co>
2021-09-09 11:37:01 -0500

    test en movies y users

---
+++ service-movies/src/test/java/co/com/poli/movies/MoviesServiceMockTest.java
@@ -0,0 +1,44 @@
+package co.com.poli.movies;
+
+import co.com.poli.movies.entities.Movie;
+import co.com.poli.movies.repositories.MovieRepository;
+import co.com.poli.movies.services.MovieService;
+import co.com.poli.movies.services.MovieServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+@SpringBootTest
+public class MoviesServiceMockTest {
+
+    @Mock
+    private MovieRepository movieRepository;
+    private MovieService movieService;
+
+    @BeforeEach
+    public void begin(){
+        MockitoAnnotations.initMocks(this);
+        movieService = new MovieServiceImpl(movieRepository);
+
+        Movie movie = Movie.builder()
+                .id(3L)
+                .tittle("testtittle")
+                .director("testdirector")
+                .rating(4)
+                .build();
+
+        Mockito.when(movieRepository.findById(3L)).thenReturn(Optional.of(movie));
+    }
+
+    @Test
+    public void when_findById_return_user(){
+        Movie movie = movieService.findByID(3L);
+        Assertions.assertThat(movie.getTittle()).isEqualTo("testtittle");
+    }
+}
